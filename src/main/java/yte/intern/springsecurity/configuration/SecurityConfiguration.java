f0619f0bda94d3af81b13c919e131f9591916838
ruchan <ruchan.yavuzdemir@icloud.com>
2023-10-16 17:25:56 +0300

    Add security filter and configuration

--- src/main/java/yte/intern/springsecurity/configuration/SecurityConfiguration.java
+++ src/main/java/yte/intern/springsecurity/configuration/SecurityConfiguration.java
@@ -1,11 +1,14 @@
 package yte.intern.springsecurity.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 @Configuration
@@ -13,14 +16,28 @@ public class SecurityConfiguration {
 
     public SecurityConfiguration(AuthenticationManagerBuilder authenticationManagerBuilder)
         throws Exception {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         UserDetails user = User.builder()
                 .username("ruchan")
-                .password("1234567890")
-                .authorities("ADMIN")
+                .password(passwordEncoder.encode("1234"))
+                .authorities("READ", "ROLE_USER")
                 .build();
 
         authenticationManagerBuilder.inMemoryAuthentication()
                 .withUser(user)
-                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+                .passwordEncoder(passwordEncoder);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.authorizeHttpRequests()
+                .requestMatchers(AntPathRequestMatcher.antMatcher("/hello")).hasAuthority("ROLE_USER")
+                .requestMatchers(AntPathRequestMatcher.antMatcher("/hi")).hasRole("READ")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .build();
+
     }
 }
